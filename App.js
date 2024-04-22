dd9425b8bf44d95b90a12fc64e4339a471616629
Diyaridem <netdiy32@hotmail.com>
2024-04-22 20:31:54 +0300

    add category

--- App.js
+++ App.js
@@ -1,30 +1,16 @@
-// App.js
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import CategoriesScreen from './screens/CategoriesScreen';
 
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import Menu from './screens/Menu';
-
-const App = () => {
-  const menuItems = [
-    { id: 1, name: 'Köfte', price: '20 TL' },
-    { id: 2, name: 'Pide', price: '15 TL' },
-    { id: 3, name: 'Salata', price: '10 TL' },
-    // Buraya menünüzdeki diğer öğeleri ekleyebilirsiniz
-  ];
-
-  return (
-    <View style={styles.container}>
-      <Menu menuItems={menuItems} />
-    </View>
-  );
-};
+export default function App() {
+  return <CategoriesScreen />;
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 40,
-    paddingHorizontal: 20,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-export default App;
