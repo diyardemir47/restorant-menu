f9dcf7888decadf77b97e8e5dff3b5e489bc9fc4
Diyaridem <netdiy32@hotmail.com>
2024-04-24 19:05:02 +0300

    food details

--- App.js
+++ App.js
@@ -4,14 +4,47 @@ import CategoriesScreen from './screens/CategoriesScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import FoodOverviewScreen from './screens/FoodOverviewScreen';
+import FoodDetailScreen from './screens/FoodDetailScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import FavoritesScreen from './screens/FavoritesScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Categories" component={CategoriesScreen} />
+      <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+    </Drawer.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Categories" component={CategoriesScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: 'blue' },
+          headerTintColor: 'white',
+          contentStyle: { backgroundColor: 'lightblue' },
+        }}
+      >
+        <Stack.Screen
+          name="Drawer"
+          component={DrawerNavigator}
+          options={{
+            headerShown: false,
+          }}
+        />
         <Stack.Screen name="FoodOverview" component={FoodOverviewScreen} />
+        <Stack.Screen
+          name="FoodDetail"
+          component={FoodDetailScreen}
+          options={{
+            title: 'İçerik',
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/FoodOverviewScreen.js
+++ screens/FoodOverviewScreen.js
@@ -1,13 +1,25 @@
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import React from 'react';
-import { FOODS } from '../data/dummy-data';
+import { FOODS, CATEGORIES } from '../data/dummy-data';
 import FoodItem from '../components/FoodItem';
+import { useLayoutEffect } from 'react';
 
-export default function FoodOverviewScreen({ route }) {
+export default function FoodOverviewScreen({ route, navigation }) {
   const categoryId = route.params.categoryId;
   const displayedFoods = FOODS.filter((foodItem) => {
     return foodItem.categoryIds.indexOf(categoryId) >= 0;
   });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === categoryId
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [navigation, categoryId]);
+
   //   console.log(displayedFoods);
 
   function renderFoodItem(itemData) {
